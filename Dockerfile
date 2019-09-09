f00d26a0b4d1a9d2f95dd1002de151c1ee667850
Marc Lough <marc@ahead4.com>
2019-09-09 17:08:39 +0100

    Update to use Postfix with MySQL for authentication

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,26 @@
-From ubuntu:trusty
-MAINTAINER Elliott Ye
+From ubuntu:eoan
 
-# Set noninteractive mode for apt-get
 ENV DEBIAN_FRONTEND noninteractive
 
-# Update
 RUN apt-get update
 
-# Start editing
-# Install package here for cache
-RUN apt-get -y install supervisor postfix sasl2-bin opendkim opendkim-tools
+RUN apt-get -y install supervisor postfix postfix-mysql libsasl2-modules-sql sasl2-bin libsasl2-2 libpam-mysql opendkim opendkim-tools rsyslog nano
 
-# Add files
 ADD assets/install.sh /opt/install.sh
 
-# Run
-CMD /opt/install.sh;/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+ADD assets/postfix/ /etc/postfix
+ADD assets/pam.d /etc/pam.d
+
+RUN chmod 640 /etc/postfix/mysql-*.cf \
+ && chgrp postfix /etc/postfix/mysql-*.cf
+
+RUN groupadd -g 5000 vmail \
+ && useradd -g vmail -u 5000 vmail -d /home/vmail -m
+
+#RUN openssl req -new -outform PEM -out /etc/postfix/smtpd.cert -newkey rsa:2048 -nodes -keyout /etc/postfix/smtpd.key -keyform PEM -days 3650 -x509 \
+# && chmod 640 /etc/postfix/smtpd.key
+
+RUN adduser postfix sasl \
+ && mkdir -p /var/spool/postfix/var/run/saslauthd
+
+CMD /opt/install.sh; /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
--- assets/install.sh
+++ assets/install.sh
@@ -14,7 +14,10 @@ nodaemon=true
 command=/opt/postfix.sh
 
 [program:rsyslog]
-command=/usr/sbin/rsyslogd -n -c3
+command=/usr/sbin/rsyslogd -n
+
+[program:saslauthd]
+command=/usr/sbin/saslauthd -d -c -m /var/spool/postfix/var/run/saslauthd -r -a pam
 EOF
 
 ############
@@ -22,109 +25,55 @@ EOF
 ############
 cat >> /opt/postfix.sh <<EOF
 #!/bin/bash
-service postfix start
-tail -f /var/log/mail.log
+#service postfix start
+#tail -f /var/log/mail.log
+postfix start-fg
 EOF
 chmod +x /opt/postfix.sh
-postconf -e myhostname=$maildomain
-postconf -F '*/*/chroot = n'
 
-############
-# SASL SUPPORT FOR CLIENTS
-# The following options set parameters needed by Postfix to enable
-# Cyrus-SASL support for authentication of mail clients.
-############
-# /etc/postfix/main.cf
-postconf -e smtpd_sasl_auth_enable=yes
-postconf -e broken_sasl_auth_clients=yes
-postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
-# smtpd.conf
-cat >> /etc/postfix/sasl/smtpd.conf <<EOF
-pwcheck_method: auxprop
-auxprop_plugin: sasldb
-mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
-EOF
-# sasldb2
-echo $smtp_user | tr , \\n > /tmp/passwd
-while IFS=':' read -r _user _pwd; do
-  echo $_pwd | saslpasswd2 -p -c -u $maildomain $_user
-done < /tmp/passwd
-chown postfix.sasl /etc/sasldb2
+postconf -e myhostname=$HOSTNAME
+#postconf -F '*/*/chroot = n'
 
-############
-# Enable TLS
-############
-if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
-  # /etc/postfix/main.cf
-  postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
-  postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
-  chmod 400 /etc/postfix/certs/*.*
-  # /etc/postfix/master.cf
-  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-  postconf -P "submission/inet/syslog_name=postfix/submission"
-  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
-  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
-  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
-  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
-fi
+postconf -e 'mydestination = localhost, proxy:mysql:/etc/postfix/mysql-mydestination.cf'
+postconf -e 'virtual_alias_maps = proxy:mysql:/etc/postfix/mysql-virtual_forwardings.cf, mysql:/etc/postfix/mysql-virtual_email2email.cf'
+postconf -e 'virtual_mailbox_domains = proxy:mysql:/etc/postfix/mysql-virtual_domains.cf'
+postconf -e 'virtual_mailbox_maps = proxy:mysql:/etc/postfix/mysql-virtual_mailboxes.cf'
+postconf -e 'virtual_mailbox_base = /home/vmail'
+postconf -e 'virtual_uid_maps = static:5000'
+postconf -e 'virtual_gid_maps = static:5000'
+postconf -e 'smtpd_sasl_auth_enable = yes'
+postconf -e 'smtpd_helo_required = yes'
+postconf -e 'broken_sasl_auth_clients = yes'
+postconf -e 'smtpd_recipient_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination'
+postconf -e 'smtpd_use_tls = yes'
+postconf -e 'smtpd_tls_cert_file = /etc/postfix/smtpd.crt'
+postconf -e 'smtpd_tls_key_file = /etc/postfix/smtpd.key'
+postconf -e 'smtpd_tls_security_level = encrypt'
+postconf -e 'smtpd_tls_auth_only = yes'
+postconf -e 'strict_rfc821_envelopes = yes'
+postconf -e 'disable_vrfy_command = yes'
+postconf -e 'transport_maps = proxy:mysql:/etc/postfix/mysql-virtual_transports.cf'
+#postconf -e 'virtual_create_maildirsize = yes'
+#postconf -e 'virtual_mailbox_extended = yes'
+postconf -e 'virtual_mailbox_limit_maps = proxy:mysql:/etc/postfix/mysql-virtual_mailbox_limit_maps.cf'
+#postconf -e 'virtual_mailbox_limit_override = yes'
+#postconf -e 'virtual_maildir_limit_message = "Account is over quota"'
+#postconf -e 'virtual_overquota_bounce = yes'
+postconf -e 'proxy_read_maps = $local_recipient_maps $mydestination $virtual_alias_maps $virtual_alias_domains $virtual_mailbox_maps    $virtual_mailbox_domains $relay_recipient_maps $relay_domains $canonical_maps $sender_canonical_maps $recipient_canonical_maps $relocated_maps $transport_maps $mynetworks $virtual_mailbox_limit_maps'
 
-#############
-#  opendkim
-#############
+CONFIG_FILES="/etc/postfix/*.cf"
+CONFIG_FILES+=" /etc/pam.d/smtp"
+CONFIG_FILES+=" /etc/postfix/sasl/smtpd.conf"
 
-if [[ -z "$(find /etc/opendkim/domainkeys -iname *.private)" ]]; then
-  exit 0
-fi
-cat >> /etc/supervisor/conf.d/supervisord.conf <<EOF
+echo $CONFIG_FILES
 
-[program:opendkim]
-command=/usr/sbin/opendkim -f
-EOF
-# /etc/postfix/main.cf
-postconf -e milter_protocol=2
-postconf -e milter_default_action=accept
-postconf -e smtpd_milters=inet:localhost:12301
-postconf -e non_smtpd_milters=inet:localhost:12301
+for file in $CONFIG_FILES
+do
+  sed -i "s/{mysql_host}/$MYSQL_HOST/g" $file
+  sed -i "s/{mysql_username}/$MYSQL_USERNAME/g" $file
+  sed -i "s/{mysql_password}/$MYSQL_PASSWORD/g" $file
+  sed -i "s/{mysql_database}/$MYSQL_DATABASE/g" $file
+done
 
-cat >> /etc/opendkim.conf <<EOF
-AutoRestart             Yes
-AutoRestartRate         10/1h
-UMask                   002
-Syslog                  yes
-SyslogSuccess           Yes
-LogWhy                  Yes
-
-Canonicalization        relaxed/simple
-
-ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
-InternalHosts           refile:/etc/opendkim/TrustedHosts
-KeyTable                refile:/etc/opendkim/KeyTable
-SigningTable            refile:/etc/opendkim/SigningTable
-
-Mode                    sv
-PidFile                 /var/run/opendkim/opendkim.pid
-SignatureAlgorithm      rsa-sha256
-
-UserID                  opendkim:opendkim
-
-Socket                  inet:12301@localhost
-EOF
-cat >> /etc/default/opendkim <<EOF
-SOCKET="inet:12301@localhost"
-EOF
-
-cat >> /etc/opendkim/TrustedHosts <<EOF
-127.0.0.1
-localhost
-192.168.0.1/24
-
-*.$maildomain
-EOF
-cat >> /etc/opendkim/KeyTable <<EOF
-mail._domainkey.$maildomain $maildomain:mail:$(find /etc/opendkim/domainkeys -iname *.private)
-EOF
-cat >> /etc/opendkim/SigningTable <<EOF
-*@$maildomain mail._domainkey.$maildomain
-EOF
-chown opendkim:opendkim $(find /etc/opendkim/domainkeys -iname *.private)
-chmod 400 $(find /etc/opendkim/domainkeys -iname *.private)
+cp /etc/services /var/spool/postfix/etc
+cp /etc/resolv.conf /var/spool/postfix/etc
